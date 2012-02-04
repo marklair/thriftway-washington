9d9db2f82e5ba56503849294a6927e44a2a363ab
Greg Hemphill <greg@webstop.com>
2012-02-03 22:22:16 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Template::Application.config.secret_token = 'eb31a651d0aabf8a2861fc79c5c8af595f512c82a31a6ad6333ce07d1a80f132de546a682535485b03ce65fbd816ae8bf72dbf89c22cd05921f01c6104cfaaf1'
