b5e7115f818bcc00be9469206d39a992f146a407
Greg Hemphill <greg@webstop.com>
2012-10-21 23:56:34 -0400

    Fixes bugs with Bootstrap Javascript and DataTables example.
    Switched to using the official combined javasctip file for Bootstrap.

--- vendor/assets/javascripts/data_tables/data_tables_for_bootstrap.js
+++ vendor/assets/javascripts/data_tables/data_tables_for_bootstrap.js
@@ -148,7 +148,7 @@ if ( $.fn.DataTable.TableTools ) {
 /* Table initialisation */
 $(document).ready(function() {
 	$('#example').dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
 		"sPaginationType": "bootstrap",
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
