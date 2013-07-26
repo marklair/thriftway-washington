28a0c9cf5248d9b3461e7d82d1a7819f11e8f193
marklair <mark@webstop.com>
2013-07-26 11:11:23 -0400

    attempt to delete unused files

--- app/models/message.rb
+++
@@ -1,3 +0,0 @@
-class Message < ActiveRecord::Base
-  validates :name, :email, :phone, :company, :message_type, :presence => true
-end
--- db/migrate/20130628152408_add_type_field_to_messages.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeFieldToMessages < ActiveRecord::Migration
-  def self.up
-    add_column :messages, :message_type, :string, :null => false, :default => 'contact'
-  end
-
-  def self.down
-    remove_column :messages, :message_type
-  end
-end
